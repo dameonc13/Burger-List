acd7c82afac4823c9fd11a69ee066f2dca548b6b
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-03-01 21:07:47 -0500

    connecting to heroku jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 const user = require('../user.js');
+var connection;
 
+if ( process.env.JAWSDB_URL){
 
-
-var connection = mysql.createConnection({
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: `${user.getName()}`,
   password: `${user.getPassword()}`,
   database: "Burgers_db"
-});
+})
+}
 
 // Make connection.
 connection.connect(function(err) {
